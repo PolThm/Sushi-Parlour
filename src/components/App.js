7874f5152c1e51e70967b229c370509b40f27d73
PolThm <pol.thomas56@gmail.com>
2020-07-20 12:43:08 +0200

    Main background done

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import "./App.css";
 import HeroArea from "./HeroArea";
+import mainBG from "../img/main-bg.png";
 
 const App = () => {
   return (
-    <React.Fragment>
+    <div className="main">
+      <img src={mainBG} alt="main background" className="main-bg"/>
       <HeroArea/>
 
       <div className="h-screen"/>
-    </React.Fragment>
+    </div>
   );
 };
 
