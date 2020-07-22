fb25503687a9306db1d18c10b5b0e76ce9340e04
PolThm <pol.thomas56@gmail.com>
2020-07-22 14:38:49 +0200

    Group03 done + update on G1 & G2.

--- src/components/Group02.js
+++ src/components/Group02.js
@@ -1,16 +1,17 @@
 import React from "react";
+import "./Group02.css";
 import imgG2 from "../img/group_02/group02-img.png"
 
 const Group02 = () => {
   return (
     <div className="flex">
-      <div className="pl-24 pr-12 flex flex-col justify-center">
+      <div className="g2-texts pr-12 py-10 flex flex-col justify-center">
         <h2 className="font-goodDogCool text-3xl text-SPGreen">What do we actually cook?</h2>
-        <p className="mt-6 font-montserrat font-light">Lorem ipsum dolor sit amet, consectetur adipisicing elit sed
+        <p className="mt-6 font-montserrat font-light text-lg">Lorem ipsum dolor sit amet, consectetur adipisicing elit sed
           do eiusmod tempor incididunt ut labore et dolorem magna
           aliqua. Ut enim ad minim veniam quis nostrud.</p>
       </div>
-      <img src={imgG2} alt="Some sushis" />
+      <img src={imgG2} alt="Some sushis"/>
     </div>
   );
 };
--- src/components/Group03.js
+++ src/components/Group03.js
@@ -1,9 +1,15 @@
 import React from "react";
+import "./Group03.css";
+import Group03Element from "./Group03Element";
 
 const Group03 = () => {
   return (
-    <div className="h-64 bg-SPDarkGreen bg-opacity-75">
-
+    <div className="group03 bg-SPDarkGreen bg-opacity-75">
+      <div className="container h-full flex justify-center items-center">
+        <Group03Element title={'New tastes'} nbOfBrushes={1}/>
+        <Group03Element title={'New dishes cooking'} nbOfBrushes={2}/>
+        <Group03Element title={'New restaurant'} nbOfBrushes={3}/>
+      </div>
     </div>
   );
 };
